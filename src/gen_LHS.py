2eb0c07604be295d710af7e2f61f7e9cd0540983
j-emberton <jamie.emberton@gmail.com>
2023-12-13 07:11:26 +0000

    Added docstrings and unit tests

--- src/gen_LHS.py
+++ src/gen_LHS.py
@@ -3,7 +3,18 @@ import pandas as pd
 from scipy.stats.distributions import norm
 
 class LHS:
+    """Generates a Latin Hypercube Sample (LHS) from a dictionary of means and standard deviations."""
+
     def __init__(self, data: dict, samples: int = 1000, criterion: str = 'maximin', iterations: int = 1000, random_state: int = None):
+        """
+        Args:
+            data (dict): Dictionary of means and standard deviations for each dimension.
+            samples (int, optional): Number of samples to generate. Defaults to 1000.
+            criterion (str, optional): Criterion to use for generating the LHS. Defaults to 'maximin'.
+            iterations (int, optional): Number of iterations to use for generating the LHS. Defaults to 1000.
+            random_state (int, optional): Random state to use for generating the LHS. Defaults to None.
+            
+            """
         self.data = data
         self.dimensions = None
         self.samples = samples
@@ -13,6 +24,7 @@ class LHS:
         self.sample_dataframe = self._parse_input_dict()
 
     def _parse_input_dict(self):
+        """Parses the input dictionary and generates a LHS."""
         column_names = self._get_dimension_names()
         self.dimensions = len(column_names)
         means, std_devs = self._extract_mean_std_dev(self.data, column_names)
@@ -20,21 +32,39 @@ class LHS:
         return df
     
     def _get_dimension_names(self):
+        """Gets the names of the dimensions from the input dictionary."""
         dimension_names = list(self.data.keys())
         return dimension_names
     
-    def _extract_mean_std_dev(self, data: dict, column_names: list):
+    def _extract_mean_std_dev(self, column_names: list):
+        """Extracts the means and standard deviations from the input dictionary.
+            
+            Args:
+                column_names (list): List of dimension names.
+                
+                Returns:
+                    means (dict): Dictionary of means for each dimension.
+                    std_devs (dict): Dictionary of standard deviations for each dimension."""
         means = {}
         std_devs = {}
 
         for n in column_names:
-            first_level_dict = data[n]
+            first_level_dict = self.data[n]
             means[n] = first_level_dict['mean']
             std_devs[n] = first_level_dict['std_dev']
 
         return means, std_devs
     
     def _gen_LHS(self, means: dict, std_devs: dict, column_names: list):
+        """Generates a LHS from the means and standard deviations.
+        
+            Args:
+                means (dict): Dictionary of means for each dimension.
+                std_devs (dict): Dictionary of standard deviations for each dimension.
+                column_names (list): List of dimension names.
+                
+                Returns:
+                    df (pd.DataFrame): DataFrame containing the LHS."""
         design = doe.lhs(self.dimensions, samples=self.samples, criterion=self.criterion, iterations=self.iterations, random_state=self.random_state)
 
         for i in range(self.dimensions):
---
+++ test/test_LHS.py
@@ -0,0 +1,50 @@
+import pytest
+import pandas as pd
+from src.gen_LHS import LHS
+
+
+# Set up data for testing
+data = {
+    'dimension1': {'mean': 0, 'std_dev': 1},
+    'dimension2': {'mean': 5, 'std_dev': 2},
+    'dimension3': {'mean': -3, 'std_dev': 0.5}
+}
+samples = 1000
+criterion = 'maximin'
+iterations = 1000
+random_state = 42
+
+# Initialize LHS object
+lhs = LHS(data, samples, criterion, iterations, random_state)
+
+# Test parsing the input dictionary
+def test_parse_input_dict():
+    assert isinstance(lhs.sample_dataframe, pd.DataFrame)
+
+# Test getting dimension names
+def test_get_dimension_names():
+    expected_dimensions = ['dimension1', 'dimension2', 'dimension3']
+    assert lhs._get_dimension_names() == expected_dimensions
+
+# Test extracting means and std_devs
+def test_extract_mean_std_dev():
+    expected_means = {'dimension1': 0, 'dimension2': 5, 'dimension3': -3}
+    expected_std_devs = {'dimension1': 1, 'dimension2': 2, 'dimension3': 0.5}
+    means, std_devs = lhs._extract_mean_std_dev(['dimension1', 'dimension2', 'dimension3'])
+    assert means == expected_means
+    assert std_devs == expected_std_devs
+
+# Test generating LHS
+def test_gen_LHS():
+    df = lhs._gen_LHS(
+        {'dimension1': 0, 'dimension2': 5, 'dimension3': -3},
+        {'dimension1': 1, 'dimension2': 2, 'dimension3': 0.5},
+        ['dimension1', 'dimension2', 'dimension3']
+    )
+    assert isinstance(df, pd.DataFrame)
+    assert df.shape == (samples, len(data))
+
+# Test invalid criterion
+def test_invalid_criterion():
+    with pytest.raises(ValueError, match="Invalid criterion. Please choose a valid criterion."):
+        LHS(data, samples, criterion='invalid_criterion')
